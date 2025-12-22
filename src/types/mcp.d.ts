0c34ab2765076707a1d7dcfe955f6045a4267096
Yoannis Sanchez Soto <yossthedev@gmail.com>
2025-12-22 17:14:08 -0500

    refactor: update package.json, tsconfig, and yarn.lock; add mcp.d.ts type declarations

---
+++ src/types/mcp.d.ts
@@ -0,0 +1,4 @@
+declare module '@modelcontextprotocol/sdk/client/index.js';
+declare module 'gtoken';
+declare module 'google-auth-library/build/src/auth/googleauth.js';
+declare module 'google-auth-library/build/src/auth/oauth2client.js'
